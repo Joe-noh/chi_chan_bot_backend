bf4b6c725f2f77d87d4e53de0c3a0c8115c8e94f
Joe-noh <goflb.jh@gmail.com>
2016-02-14 01:15:13 +0900

    render_xxx spec changed

--- web/views/photo_view.ex
+++ web/views/photo_view.ex
@@ -2,11 +2,11 @@ defmodule ChiChan.PhotoView do
   use ChiChan.Web, :view
 
   def render("index.json", %{photos: photos}) do
-    %{data: render_many(photos, "photo.json")}
+    %{data: render_many(photos, __MODULE__, "photo.json")}
   end
 
   def render("show.json", %{photo: photo}) do
-    %{data: render_one(photo, "photo.json")}
+    %{data: render_one(photo, __MODULE__, "photo.json")}
   end
 
   def render("photo.json", %{photo: photo}) do
